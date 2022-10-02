bfa34a3ef649bc8395df1e20b96c2bf0ce5d63e2
henrique.castro <henrique.castro@pattrol.com.br>
2022-10-02 18:23:17 -0300

    One Drawer to rule them all 💍

--- simulador-so-web/src/view/Simulador.tsx
+++ simulador-so-web/src/view/Simulador.tsx
@@ -1,36 +1,20 @@
 import * as React from 'react';
-import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import AddIcon from '@mui/icons-material/Add';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import StopCircleIcon from '@mui/icons-material/StopCircle';
-import DiscreteSlider from './slider';
-import DeleteIcon from '@mui/icons-material/Delete';
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
-import HourglassTopIcon from '@mui/icons-material/HourglassTop';
 import pixelToRem from './utils/pxToRem';
-import { main, start, stop, cpu } from '../simulator/main';
+import { main, cpu } from '../simulator/main';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 
 import { Container, SimulatorTitle, SimulatorCanvas, MenuTitles } from './estilos/styles';
 import AddProcessForm from './MainPage/AddProcessForm';
 import ProcessList from './MainPage/ProcessList';
+import DrawerMenu from './MainPage/DrawerMenu';
 
 //TEMA DO CSS DA PÁGINA
 const darkTheme = createTheme({
@@ -48,36 +32,6 @@ function useForceUpdate() {
 
 //CONFIGURAÇÕES DO DRAWER(MENU) LATERAL
 const drawerWidth = 240;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
 
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean;
@@ -100,23 +54,6 @@ const AppBar = styled(MuiAppBar, {
     }),
   }),
 }));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: prop => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  })
-);
 
 //MENU(DRAWER) LATERAL
 export default function Simulador() {
@@ -144,37 +81,8 @@ export default function Simulador() {
     setCpuState(cpu);
   });
 
-  const theme = useTheme();
-
   const handleDrawerOpen = () => {
     setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  //FUNÇÃO QUE CONFERE O TEXTO DO BOTÃO DO MENU LATERAL PARA ASSOCIÁ-LO À FUNÇÃO CERTA DO SIMULADOR
-  const checkMenuText = (text: string) => {
-    if (text === 'Iniciar') {
-      start();
-    }
-    if (text === 'Parar') {
-      stop();
-    }
-    if (text === 'Adicionar') {
-      handleAddModalOpen();
-    }
-    if (text === 'Suspender') {
-      return cpu.addProcess(null, 4, 4);
-    }
-    if (text === 'Excluir') {
-      return cpu.addProcess(100000);
-    }
-    if (text === 'Listar') {
-      handleListModalOpen();
-    }
-    return 0;
   };
 
   return (
@@ -198,129 +106,18 @@ export default function Simulador() {
             <Typography>SIMULADOR DE SISTEMA OPERACIONAL MULTICORE</Typography>
           </Toolbar>
         </AppBar>
-
         {/* MODAL QUE ABRE EM ADICIONAR PROCESSO */}
         <AddProcessForm open={openAddModal} handleClose={setAddModalOpen} />
-
         {/* MODAL QUE ABRE EM LISTAR PROCESSOS */}
         <ProcessList open={openListModal} handleClose={setListModalOpen} />
 
-        <Drawer variant="permanent" open={open}>
-          <DrawerHeader>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>
-          </DrawerHeader>
-          <Divider />
+        <DrawerMenu
+          open={open}
+          handleClose={setOpen}
+          handleAddModalOpen={handleAddModalOpen}
+          handleListModalOpen={handleListModalOpen}
+        />
 
-          {/* BOTÕES DE SIMULAÇÃO LIGADOS ÀS SUAS RESPECTIVAS FUNÇÕES */}
-          <List>
-            {open ? (
-              <Typography style={{ marginLeft: 23, marginTop: 10, fontWeight: 'bold' }}>
-                SIMULAÇÃO
-              </Typography>
-            ) : null}
-            {['Iniciar', 'Parar'].map((text, index) => (
-              <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                <ListItemButton
-                  sx={{
-                    minHeight: 48,
-                    justifyContent: open ? 'initial' : 'center',
-                    px: 2.5,
-                  }}
-                  onClick={() => checkMenuText(text)}
-                >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 2 : 'auto',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    {index === 0 ? <PlayCircleOutlineIcon onClick={start} /> : null}
-                    {index === 1 ? <StopCircleIcon onClick={stop} /> : null}
-                  </ListItemIcon>
-                  <MenuTitles>{open ? text : null}</MenuTitles>
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-
-          {/* BOTÕES DE PROCESSO LIGADOS ÀS SUAS RESPECTIVAS FUNÇÕES */}
-          <List>
-            {open ? (
-              <Typography style={{ marginLeft: 23, marginTop: 10, fontWeight: 'bold' }}>
-                PROCESSOS
-              </Typography>
-            ) : null}
-            {['Adicionar', 'Suspender', 'Excluir', 'Listar'].map((text, index) => (
-              <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                <ListItemButton
-                  sx={{
-                    minHeight: 48,
-                    justifyContent: open ? 'initial' : 'center',
-                    px: 2.5,
-                  }}
-                  onClick={() => checkMenuText(text)}
-                >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 2 : 'auto',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    {index === 0 ? <AddIcon onClick={() => handleAddModalOpen()} /> : null}
-                    {index === 1 ? (
-                      <HourglassTopIcon onClick={() => cpu.addProcess(null, 4, 4)} />
-                    ) : null}
-                    {index === 2 ? <DeleteIcon onClick={() => cpu.addProcess(100000)} /> : null}
-                    {index === 3 ? (
-                      <FormatListBulletedIcon onClick={() => handleListModalOpen()} />
-                    ) : null}
-                  </ListItemIcon>
-                  <MenuTitles>{open ? text : null}</MenuTitles>
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-
-          {/* SLIDER DE CONFIGURAÇÃO DO TEMPO DE ROUND-ROBIN */}
-          <List>
-            {open ? (
-              <Typography
-                style={{ marginLeft: 23, marginTop: 10, marginBottom: 10, fontWeight: 'bold' }}
-              >
-                ROUND-ROBIN
-              </Typography>
-            ) : null}
-            {['Ajustar tempo'].map((text, index) => (
-              <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                <ListItem
-                  sx={{
-                    minHeight: 48,
-                    justifyContent: open ? 'initial' : 'center',
-                    px: 2.5,
-                  }}
-                >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 2 : 'auto',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    {index === 0 ? <AccessTimeIcon /> : null}
-                  </ListItemIcon>
-                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-                </ListItem>
-              </ListItem>
-            ))}
-          </List>
-          {open ? <DiscreteSlider /> : null}
-        </Drawer>
         {/* FORA DO MENU(DRAWER) - CANVAS DO SIMULADOR EM SI */}
         <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
           <Container flex="column" margin={pixelToRem(70, 112, 50)}>
