0007164a76af3c5f1c29d00f17bfa235fe48c5fe
Henrique <henrique.castros@outlook.com>
2022-10-06 22:47:00 -0300

    error log 💇‍♂️

--- simulador-so-web/src/view/Forms.tsx
+++ simulador-so-web/src/view/Forms.tsx
@@ -1,12 +1,14 @@
 import { useForm, Controller } from 'react-hook-form';
 import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
+import { Alert, Collapse, TextField } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import { cpuContext } from './context/CpuContext';
 import React from 'react';
 
 export default function BasicTextFields(this: any) {
   const { cpu, forceUpdate } = React.useContext(cpuContext);
+  const [openAlert, setOpenAlert] = React.useState(false);
+  const [message, setMessage] = React.useState<any>('');
   const { control, handleSubmit } = useForm({
     defaultValues: {
       executionSize: null,
@@ -18,73 +20,81 @@ export default function BasicTextFields(this: any) {
     try {
       cpu?.addProcess(data);
     } catch (e) {
-      alert(e);
+      setOpenAlert(true);
+      setMessage(e);
     }
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name="memorySize"
-        control={control}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            required
-            type="number"
-            id="outlined-basic"
-            label="Tamanho de memória"
-            variant="outlined"
-            style={{ width: '37ch', marginBottom: 20 }}
-            color="secondary"
-          />
-        )}
-      />
-      <Controller
-        name="priority"
-        control={control}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            required
-            type="number"
-            id="outlined-basic"
-            label="Prioridade"
-            variant="outlined"
-            style={{ width: '37ch', marginBottom: 20 }}
-            color="secondary"
-          />
-        )}
-      />
-      <Controller
-        name="executionSize"
-        control={control}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            type="number"
-            id="outlined-basic"
-            label="Tempo que vai rodar"
-            variant="outlined"
-            style={{ width: '37ch', marginBottom: 10 }}
-            color="secondary"
-          />
-        )}
-      />
-      <Stack spacing={2} direction="column">
-        <Button
-          type="submit"
-          variant="contained"
-          style={{
-            marginTop: 20,
-            color: 'white',
-            backgroundColor: '#680079',
-            fontWeight: 'bold',
-            width: '42ch',
-          }}
-        >
-          ADICIONAR
-        </Button>
-      </Stack>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="memorySize"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              required
+              type="number"
+              id="outlined-basic"
+              label="Tamanho de memória"
+              variant="outlined"
+              style={{ width: '37ch', marginBottom: 20 }}
+              color="secondary"
+            />
+          )}
+        />
+        <Controller
+          name="priority"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              required
+              type="number"
+              id="outlined-basic"
+              label="Prioridade"
+              variant="outlined"
+              style={{ width: '37ch', marginBottom: 20 }}
+              color="secondary"
+            />
+          )}
+        />
+        <Controller
+          name="executionSize"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              type="number"
+              id="outlined-basic"
+              label="Tempo que vai rodar"
+              variant="outlined"
+              style={{ width: '37ch', marginBottom: 10 }}
+              color="secondary"
+            />
+          )}
+        />
+        <Collapse in={openAlert}>
+          <Alert variant="outlined" severity="error" sx={{ marginTop: 1 }}>
+            {message}
+          </Alert>
+        </Collapse>
+        <Stack spacing={2} direction="column">
+          <Button
+            type="submit"
+            variant="contained"
+            style={{
+              marginTop: 20,
+              color: 'white',
+              backgroundColor: '#680079',
+              fontWeight: 'bold',
+              width: '42ch',
+            }}
+          >
+            ADICIONAR
+          </Button>
+        </Stack>
+      </form>
+    </>
   );
 }
